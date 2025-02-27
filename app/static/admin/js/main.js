4e7bedc8671f84023c8d326541db284dac408db8
Rumpel <im.denis@icloud.com>
2025-02-27 23:09:38 +0400

    работа с меню

--- app/static/admin/js/main.js
+++ app/static/admin/js/main.js
@@ -1,4 +1,3 @@
-// JavaScript
 function updateTime() {
     fetch('/time')
         .then(response => response.json())
@@ -10,11 +9,15 @@ function updateTime() {
         .catch(error => console.error('Ошибка:', error));
 }
 
-// Первый запуск сразу при загрузке страницы
 document.addEventListener('DOMContentLoaded', updateTime);
 
-// Обновление каждую секунду
 const timer = setInterval(updateTime, 1000);
-
 // Опционально: остановка таймера при необходимости
 // clearInterval(timer);
+
+$(document).ready(function() {
+    $('#hide-menu').on('click', function(e) {
+        e.preventDefault(); // Отменяем стандартное действие ссылки
+        console.log("1")
+    });
+});
